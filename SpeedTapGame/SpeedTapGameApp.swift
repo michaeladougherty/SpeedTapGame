affb01d21bd6d0d6f9f648d521804fbee5e866e6
Michael Dougherty <mikedoc1111@gmail.com>
2022-12-05 13:27:34 -0500

    Initial Commit

---
+++ SpeedTapGame/SpeedTapGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeedTapGameApp.swift
+//  SpeedTapGame
+//
+//  Created by  on 12/5/22.
+//
+
+import SwiftUI
+
+@main
+struct SpeedTapGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
